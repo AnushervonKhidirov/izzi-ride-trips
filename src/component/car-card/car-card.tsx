ee418e73719e473adb3245ad67eed02d899f1f3b
Anushervon Khidirov <ankhid98@gmail.com>
2024-11-06 17:33:15 +0500

    car card added

--- src/component/car-card/car-card.tsx
+++ src/component/car-card/car-card.tsx
@@ -2,14 +2,16 @@ import type { FC } from 'react'
 import type { AdditionalProps } from '@type/common'
 import type { TCar } from '@type/car'
 
+import Image, { StaticImageData } from 'next/image'
 import Card from '@common/card/card'
 import { LinkButton } from '@common/button/button'
 
+import carImage from '@public/images/car.png'
 import { Page } from '@constant/links'
-
-import classNames from 'classnames'
-import classes from './car-card.module.css'
 import { linkGenerator } from '@helper/link-generator'
+import classNames from 'classnames'
+
+import classes from './car-card.module.css'
 
 type TCarProps = {
     car: TCar
@@ -20,15 +22,18 @@ const CarCard: FC<AdditionalProps<TCarProps>> = ({ car, className }) => {
 
     return (
         <Card className={classNames(classes.car_card, className)}>
-            <div className={classes.image}></div>
+            <CarImage image={carImage} title={car.brand} />
 
             <About car={car} />
 
-            <LinkButton href={addTripLink} title="Add trip" className={classes.add_trip_btn}>
-                Add trip
-            </LinkButton>
+            <LinkButton href={addTripLink} title="Add trip" className={classes.add_trip_btn} />
         </Card>
     )
+}
+
+const CarImage: FC<{ title: string; image: StaticImageData | string }> = ({ title, image }) => {
+    const imageData = typeof image === 'object' ? image : { src: image, width: 300, height: 300 }
+    return <Image src={imageData.src} width={imageData.width} height={imageData.height} alt={title} className={classes.image} />
 }
 
 const About: FC<TCarProps> = ({ car }) => {
