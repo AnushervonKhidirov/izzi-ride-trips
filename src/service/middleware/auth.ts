ab1626eb97197c17e4a29b0ca399f29c091f28e8
Anushervon Khidirov <ankhid98@gmail.com>
2024-11-09 12:16:28 +0500

    replaced

--- src/service/middleware/auth.ts
+++ src/service/middleware/auth.ts
@@ -6,7 +6,7 @@ import { cookies } from 'next/headers'
 import { getUserWithRefresh } from '../../api/auth-api'
 import { Headers } from '@constant/headers'
 import { Page } from '@constant/links'
-import { Token } from '../../util/constant/auth'
+import { Token } from '../../util/constant/request'
 
 export function authMiddleware(nextMiddleware: NextMiddleware): NextMiddleware {
     return async (request, event) => {
