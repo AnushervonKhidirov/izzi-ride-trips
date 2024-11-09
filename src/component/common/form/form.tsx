e9c5a29c376970d8a384eff1c037e3f22d14f1c4
Anushervon Khidirov <ankhid98@gmail.com>
2024-11-09 21:25:56 +0500

    file input added

--- src/component/common/form/form.tsx
+++ src/component/common/form/form.tsx
@@ -1,10 +1,11 @@
 'use client'
-import type { FC } from 'react'
+import type { ChangeEvent, FC } from 'react'
 import type { TextFieldProps } from '@mui/material'
 import type { TForm } from '@type/form'
 
+import { useRef, useState } from 'react'
 import { FormControl, InputLabel, OutlinedInput } from '@mui/material'
-import { FormBtn } from '@common/button/button'
+import { FormBtn, LinkButton } from '@common/button/button'
 import { PasswordInput } from '@common/input/input'
 
 import classNames from 'classnames'
@@ -30,16 +31,17 @@ const Form: FC<TForm> = ({ inputs, className, loading = false, buttonText = 'sub
     )
 }
 
-const Field: FC<TextFieldProps> = ({ name, type, label, required }) => {
+const Field: FC<TextFieldProps> = ({ name, type = 'text', label, required }) => {
     const InputVariants: { [key: string]: FC } = {
         password: PasswordInput,
+        file: FilePicker,
         default: OutlinedInput,
     }
 
-    const Input: FC<TextFieldProps> = type && InputVariants[type] ? InputVariants[type] : InputVariants.default
+    const Input: FC<TextFieldProps> = type in InputVariants ? InputVariants[type] : InputVariants.default
 
     return (
-        <FormControl>
+        <FormControl className={classes.field}>
             <InputLabel size="small" htmlFor={name} style={{ fontSize: '1em' }}>
                 {required ? `${label} *` : label}
             </InputLabel>
@@ -53,6 +55,37 @@ const Field: FC<TextFieldProps> = ({ name, type, label, required }) => {
                 style={{ fontSize: '1em' }}
             />
         </FormControl>
+    )
+}
+
+const FilePicker: FC<TextFieldProps> = ({ id, name, required }) => {
+    const inputRef = useRef<HTMLInputElement>(null)
+    const [fileName, setFileName] = useState<string | null>(null)
+
+    function choseFile() {
+        inputRef.current?.click()
+    }
+
+    function getFileData(e: ChangeEvent<HTMLInputElement>) {
+        const files = e.target.files
+        if (files) setFileName(files[0].name)
+    }
+
+    return (
+        <div className={classes.file_field}>
+            <LinkButton title="Add Car Image" onClick={choseFile} className={classes.file_btn} />
+            {fileName && <div>File name: {fileName}</div>}
+
+            <input
+                id={id}
+                name={name}
+                required={required}
+                ref={inputRef}
+                type="file"
+                onChange={e => getFileData(e)}
+                className={classes.file_input}
+            />
+        </div>
     )
 }
 
