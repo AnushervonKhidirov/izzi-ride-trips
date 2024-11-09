13258b1a00187ef8c71fb39c75ce6d09c7ccf900
Anushervon Khidirov <ankhid98@gmail.com>
2024-11-09 17:49:21 +0500

    middleware fixed

--- src/service/middleware/auth.ts
+++ src/service/middleware/auth.ts
@@ -1,12 +1,10 @@
 import type { NextMiddleware } from 'next/server'
 
-import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 import Auth from '@service/auth/auth'
 
 import { Headers } from '@constant/headers'
-import { Page } from '@constant/links'
 import { Token } from '../../util/constant/request'
 
 export function authMiddleware(nextMiddleware: NextMiddleware): NextMiddleware {
@@ -24,9 +22,7 @@ export function authMiddleware(nextMiddleware: NextMiddleware): NextMiddleware {
                 refreshToken: refreshToken,
             })
 
-            if (err) return NextResponse.redirect(new URL(Page.LogIn, request.url))
-
-            request.headers.set(Headers.User, JSON.stringify(user))
+            if (!err) request.headers.set(Headers.User, JSON.stringify(user))
         }
 
         return nextMiddleware(request, event)
