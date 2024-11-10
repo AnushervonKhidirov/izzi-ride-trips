8170262765ceafaba8fe8ff6ae6838f6cb0fbd55
Anushervon Khidirov <ankhid98@gmail.com>
2024-11-10 11:12:00 +0500

    type added

--- src/util/type/common.ts
+++ src/util/type/common.ts
@@ -4,3 +4,9 @@ export type AdditionalProps<T> = T &
     PropsWithChildren & {
         className?: string
     }
+
+export type TProperty = {
+    key: string
+    name: string
+    value: string | number
+}
