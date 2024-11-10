f92a2e1b9f5639893b780ee78db805f8bfd9edb6
Anushervon Khidirov <ankhid98@gmail.com>
2024-11-10 11:10:56 +0500

    card component updated

--- src/component/common/card/card.tsx
+++ src/component/common/card/card.tsx
@@ -1,14 +1,20 @@
-import type { FC, PropsWithChildren } from 'react'
+import type { ElementType, FC } from 'react'
 
 import { Card as MuiCard } from '@mui/material'
+import { AdditionalProps } from '@type/common'
 
-type TCard = PropsWithChildren & {
-    className?: string
-}
+type TCard = AdditionalProps<{
+    tag?: ElementType
+}>
 
-const Card: FC<TCard> = ({ children, className }) => {
+const Card: FC<TCard> = ({ tag = 'div', children, className }) => {
     return (
-        <MuiCard className={className} elevation={3} sx={{ padding: '1em', backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        <MuiCard
+            component={tag}
+            className={className}
+            elevation={3}
+            sx={{ padding: '1em', backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+        >
             {children}
         </MuiCard>
     )
