17e28a7f19b8a89b0589935a6650bac310b8b792
Anushervon Khidirov <ankhid98@gmail.com>
2024-11-10 17:59:21 +0500

    car types array added

---
+++ src/util/constant/car.ts
@@ -0,0 +1,11 @@
+const carTypes = [
+    'Off-road vehicles',
+    'Minivans',
+    'Luxury saloon / Full-size luxury',
+    'City car / Minicompact',
+    'Supermini / Subcompact',
+    'Executive / Full-size',
+    'Small family / Compac',
+    'Large family / Mid-size',
+    'Sports',
+]
