c9ffc9b0330e8190f87a09276077a2264c21ff0b
Anushervon Khidirov <ankhid98@gmail.com>
2024-12-17 17:16:59 +0500

    Chat input updated

--- src/component/chat/chat-input/chat-input.tsx
+++ src/component/chat/chat-input/chat-input.tsx
@@ -3,6 +3,7 @@ import type { FC } from 'react'
 import type { AdditionalProps } from '@type/common'
 import type { TFormSubmit } from '@type/form'
 
+import { TextField } from '@mui/material'
 import { Button } from '@common/button/button'
 
 import classes from './chat-input.module.css'
@@ -11,7 +12,12 @@ import classNames from 'classnames'
 const ChatInput: FC<AdditionalProps<{ onSubmit: TFormSubmit }>> = ({ onSubmit, className }) => {
     return (
         <form onSubmit={e => onSubmit(e)} className={classNames(classes.chat_input, className)}>
-            <input name='message' type="text" placeholder="Message" />
+            <TextField
+                name="message"
+                placeholder="Message"
+                multiline
+                slotProps={{ input: { sx: { padding: '0.5em', fontSize: '1em' } } }}
+            />
             <Button title="Send" type="submit" />
         </form>
     )
