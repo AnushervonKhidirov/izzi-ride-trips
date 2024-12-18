b7867a047a45c924a11025414cff217b32051e57
Anushervon Khidirov <ankhid98@gmail.com>
2024-12-18 09:14:25 +0500

    profile button type updated

--- src/component/common/profile-btn/profile-btn.tsx
+++ src/component/common/profile-btn/profile-btn.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 import type { TUser } from '@type/auth'
+import type { AdditionalProps } from '@type/common'
 
 import Link from 'next/link'
 import { AccountCircle } from '@mui/icons-material'
@@ -10,14 +11,10 @@ import { Page } from '@constant/links'
 import { Color } from '@constant/colors'
 import classes from './profile-btn.module.css'
 
-type TProfileButton = TUser & {
-    className?: string
-}
-
-const ProfileButton: FC<TProfileButton> = ({ firstName, lastName, className }) => {
+const ProfileButton: FC<AdditionalProps<TUser>> = ({ firstName, lastName, className }) => {
     return (
         <Tooltip title="Profile" followCursor>
-            <Link href={ Page.Profile} className={classNames(classes.profile_btn, className)}>
+            <Link href={Page.Profile} className={classNames(classes.profile_btn, className)}>
                 <AccountCircle
                     className={classes.profile_icon}
                     style={{ width: '100%', height: '100%', fill: Color.Secondary }}
