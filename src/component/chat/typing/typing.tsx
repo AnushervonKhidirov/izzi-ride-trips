7792557d4f3da37cf163d155566486e8eadc6209
Anushervon Khidirov <ankhid98@gmail.com>
2024-12-18 12:40:47 +0500

    typing component added

---
+++ src/component/chat/typing/typing.tsx
@@ -0,0 +1,13 @@
+import classes from './typing.module.css'
+
+const Typing = () => {
+    return (
+        <>
+            typing <span className={classes.dot}>.</span>
+            <span className={classes.dot}>.</span>
+            <span className={classes.dot}>.</span>
+        </>
+    )
+}
+
+export default Typing
