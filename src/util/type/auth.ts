6bbab4e10fb260488f69cdfe6f7d29850c2e0f2b
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-07 09:17:22 +0500

    new type added

--- src/util/type/auth.ts
+++ src/util/type/auth.ts
@@ -16,3 +16,10 @@ export type TLogInData = {
     username: string
     password: string
 }
+
+export type TResponse<T> = {
+    code: number
+    show_custom_message: boolean
+    message: string
+    data?: T
+}
